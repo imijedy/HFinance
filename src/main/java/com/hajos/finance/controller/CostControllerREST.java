a2e96afbd22bd0d5decac879a6980fe68281f4ee
Hajós Imre <imijedy@gmail.com>
2021-02-03 15:28:34 +0100

    crosorigin

--- src/main/java/com/hajos/finance/controller/CostControllerREST.java
+++ src/main/java/com/hajos/finance/controller/CostControllerREST.java
@@ -20,65 +20,79 @@ public class CostControllerREST {
         this.costService = costService;
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getcosts", method= RequestMethod.GET)
     public List<Cost> getCosts(){
         return costService.getAllCosts();
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getcost/{id}", method= RequestMethod.GET)
     public Cost getCostById(@PathVariable Long id){
         return costService.getCost(id).get();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/deletecost/{id}", method= RequestMethod.GET)
     public void deleteCostById(@PathVariable Long id){
         costService.deleteCost(id);
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/deletecosttype/{id}", method= RequestMethod.GET)
     public void deleteCostTypeById(@PathVariable Long id){
         costService.deleteCostType(id);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getcosttypes", method= RequestMethod.GET)
     public List<CostType> getCostTypes(){
         return costService.getAllCostTypes();
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getcosttypcecategories", method= RequestMethod.GET)
     public List<CostTypeCategory> getCostTypeCategories(){
         return costService.getAllCostTypeCategories();
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getcosttype/{id}", method= RequestMethod.GET)
     public CostType getCostTypeById(@PathVariable Long id){
         return costService.getCostType(id).get();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getcosttypecategory/{id}", method= RequestMethod.GET)
     public CostTypeCategory getCostTypeCategoryById(@PathVariable Long id){
         return costService.getCostTypeCategory(id).get();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getcostsbytype/{id}", method= RequestMethod.GET)
     public List<Cost> getCostsByType(@PathVariable Long id){
         return costService.getCostsByType(id);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/addcost", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addCost(@RequestBody Cost newCost) {
         costService.addCost(newCost);
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/addcosttype", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addCostType(@RequestBody CostType newCostType) {
         costService.addCostType(newCostType);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getsumfullyearcost", method= RequestMethod.GET)
     public Double getSumFullYearCost(){
         return costService.getFullYearCostSummary();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getsummonthlycost", method= RequestMethod.GET)
     public Double getSumMonthlyCost(){
         return costService.getMonthlyCostSummary();
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getchartcost", method= RequestMethod.GET)
     public List<ChartData> getChartCost(){
         return costService.getChartCost();
--- src/main/java/com/hajos/finance/controller/IncomeControllerREST.java
+++ src/main/java/com/hajos/finance/controller/IncomeControllerREST.java
@@ -19,31 +19,37 @@ public class IncomeControllerREST {
         this.incomeService = incomeService;
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getincome/{id}", method= RequestMethod.GET)
     public Income getIncomeById(@PathVariable Long id){
         return incomeService.getIncome(id).get();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getincome", method= RequestMethod.GET)
     public List<Income> getIncome(){
         return incomeService.getAllIncome();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getsumfullyearincome", method= RequestMethod.GET)
     public Double getSumFullYearIncome(){
         return incomeService.getFullYearIncomeSummary();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getsummonthlyincome", method= RequestMethod.GET)
     public Double getSumMonthlyIncome(){
         return incomeService.getMonthlyIncomeSummary();
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/deleteincome/{id}", method= RequestMethod.GET)
     public void deleteIncomeById(@PathVariable Long id){
         incomeService.deleteIncome(id);
     }
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/addincome", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addIncome(@RequestBody Income newIncome) {
         incomeService.addIncome(newIncome);
     }
-
+    @CrossOrigin(origins = "*")
     @RequestMapping(path="/getchartincome", method= RequestMethod.GET)
     public List<ChartData> getChartIncome(){
         return incomeService.getChartIncome();
